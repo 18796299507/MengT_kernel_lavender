c7bebc19d132a41071335b670484abcf2ab579cc
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2019-04-15 23:30:52 +0900

    Revert "cpufreq: Yankactive Governor"
    This reverts commit 1809163ad23d19ee6780b4a22e98fbbac794df02.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -527,9 +527,6 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
-extern struct cpufreq_governor cpufreq_gov_yankactive;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
