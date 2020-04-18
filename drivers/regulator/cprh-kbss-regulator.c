dee522fea00992d0863ea5c0ef725571eefd9c16
Ratoriku <a1063021545@gmail.com>
2020-04-18 11:03:36 +0800

    patch: CPU&GPU Undervolt
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- drivers/regulator/cprh-kbss-regulator.c
+++ drivers/regulator/cprh-kbss-regulator.c
@@ -481,18 +481,18 @@ msm8998_v2_kbss_fuse_ref_volt[2][MSM8998_KBSS_FUSE_CORNERS] = {
 static const int
 sdm660_kbss_fuse_ref_volt[2][SDM660_KBSS_FUSE_CORNERS] = {
 	[CPRH_KBSS_POWER_CLUSTER_ID] = {
+		554000,
 		644000,
-		724000,
-		788000,
-		868000,
-		1068000,
+		718000,
+		808000,
+		1008000,
 	},
 	[CPRH_KBSS_PERFORMANCE_CLUSTER_ID] = {
-		724000,
-		788000,
-		868000,
-		988000,
-		1068000,
+		644000,
+		718000,
+		808000,
+		928000,
+		1008000,
 	},
 };
 
